6adea808292a92fbcd8b49263c214e825bca3b34
이휘련 <dlgnlfus@gmail.com>
2023-10-16 10:44:54 +0900

    숫자 맞추기 게임 추가

---
+++ ConsoleAppTC/ConsoleApp/Game/NumberGuessingGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Game {
+    internal class NumberGuessingGame : IGame {
+        public void Play()
+        {
+            Console.WriteLine("숫자 추측 게임에 오신 것을 환영합니다!");
+            Console.WriteLine("1부터 100 사이의 숫자를 생각하겠습니다. 당신은 그 숫자를 맞혀야 합니다.");
+
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+            int userGuess;
+            int attempts = 0;
+
+            do {
+                Console.Write("당신의 추측을 입력하세요: ");
+                var input = Console.ReadLine();
+                if (input?.ToLower() == "종료") {
+                    Console.WriteLine("숫자 추측 게임을 종료합니다.");
+                    return;
+                }
+
+                var isNumber = int.TryParse(input, out userGuess);
+                if (!isNumber) {
+                    Console.WriteLine("잘못된 입력입니다. 숫자를 입력해주세요.");
+                    continue;
+                }
+
+                attempts++;
+
+                if (userGuess < secretNumber) {
+                    Console.WriteLine("높게!");
+                } else if (userGuess > secretNumber) {
+                    Console.WriteLine("낮게!");
+                } else if (input.ToLower() == "종료") {
+                    Console.WriteLine("숫자 추측 게임을 종료합니다.");
+                    return;
+                }
+
+            } while (userGuess != secretNumber);
+
+            Console.WriteLine($"축하합니다! {attempts}번 만에 맞혔습니다.");
+        }
+    }
+}
--- ConsoleAppTC/ConsoleApp/Program.cs
+++ ConsoleAppTC/ConsoleApp/Program.cs
@@ -5,16 +5,23 @@ namespace ConsoleApp {
     internal class Program {
         static void Main(string[] args)
         {
-            Console.WriteLine("어떤 게임을 하시겠습니까?");
-            Console.WriteLine("1: 가위바위보");
-            Console.WriteLine("0: 종료");
-
             bool isRunning = true;
             while (isRunning) {
+                Console.WriteLine("어떤 게임을 하시겠습니까?");
+                Console.WriteLine("1: 가위바위보");
+                Console.WriteLine("2: 숫자 추측 게임");
+                Console.WriteLine("0: 종료");
+                Console.WriteLine("");
+
                 switch (Console.ReadLine()) {
                     case "1":
                         IGame rpsGame = new RockPaperScissors();
                         rpsGame.Play();
+                        break;
+
+                    case "2":
+                        IGame numberGame = new NumberGuessingGame();
+                        numberGame.Play();
                         break;
 
                     case "0":
@@ -26,6 +33,7 @@ namespace ConsoleApp {
                         Console.WriteLine("잘못된 선택입니다. 다시 선택해주세요.");
                         break;
                 }
+                Console.WriteLine("");
             }
         }
     }
